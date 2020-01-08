a19bd49a510cfd9312bba76996c888d9c1f01c6e
Deep Shah <deep@makkajai.com>
2020-01-08 14:38:49 +0530

    AA| Basic physics game <Deep>

---
+++ Archanoid/Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] GameObject paddle;
+
+    bool isNotStarted = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            isNotStarted = false;
+            var body = GetComponent<Rigidbody2D>();
+            body.AddForce(transform.up * 10, ForceMode2D.Impulse);
+        }
+        if(isNotStarted)
+        {
+            transform.position = new Vector3(paddle.transform.position.x, paddle.transform.position.y, paddle.transform.position.z);
+        }
+    }
+}
---
+++ Archanoid/Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] int screenWidthUnits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Input.mousePosition.x / Screen.width * screenWidthUnits, transform.position.y, transform.position.z);
+        //Debug.Log(Input.mousePosition.x / Screen.width * screenWidthUnits);
+        
+    }
+}
